16a715f5203a287a74488b99ebba53765d53aaab
Albert De La Fuente <vonpupp@gmail.com>
2012-04-21 17:06:36 -0300

    added random speed and positions for bugs

--- Bug.dart
+++ Bug.dart
@@ -2,12 +2,14 @@ class Bug {
   
   double x;
   double y;
+  double speed;
   ImageElement imgtag;
   test01 game;
   
   Bug(this.game, String image_source) {
-    x = 300.0;
+    x = Math.random() * 800;
     y = 100.0;
+    speed = 10 + Math.random() * 15;
     
     imgtag = new Element.tag('img');
     imgtag.attributes['src'] = image_source;
@@ -20,7 +22,24 @@ class Bug {
   }
   
   move() {
-    y += 10;
+/*    if (Math.x - game.logo.x) {
+      return;
+    }
+    if (y + 10 < game.logo.y) {
+      return;
+    }*/
+    if (game.logo.x > x) {
+      x += speed;
+    }
+    if (game.logo.x < x) {
+      x -= speed;
+    }
+    if (game.logo.y > y) {
+      y += speed;
+    }  
+    if (game.logo.y < y) {
+      y -= speed;
+    }      
     Util.pos(imgtag, x, y);
   }
   
