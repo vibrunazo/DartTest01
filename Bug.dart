ea37e45123f656dfb356a0972f09007bd7b5e8c9
Albert De La Fuente <vonpupp@gmail.com>
2012-04-22 13:17:09 -0300

    added new bugs

--- Bug.dart
+++ Bug.dart
@@ -3,8 +3,17 @@ class Bug {
   double x;
   double y;
   double speed;
-  ImageElement imgtag;
   test01 game;
+  
+  static final List<String> bugtypeimgs = const  ["img/bug01-01.png", "img/bug01-02.png",
+                                                  "img/bug02-01.png", "img/bug02-02.png",    
+                                                  "img/bug03-01.png", "img/bug03-02.png",    
+                                                  "img/bug04-01.png", "img/bug04-02.png"];    
+  ImageElement imgtag;
+  int imgindex;
+  int imganimate;
+  double imgdblindex, bugtype;
+
   
   Bug(this.game, String image_source) {
     x = Math.random() * 600;
@@ -12,7 +21,14 @@ class Bug {
     speed = 5 + Math.random() * 10;
     
     imgtag = new Element.tag('img');
-    imgtag.attributes['src'] = image_source;
+    imgindex = 0;
+    bugtype = Math.random() * 4;
+    bugtype = bugtype % 4;
+    imgindex = (bugtype * 2).toInt();
+    imgtag.attributes['src'] = bugtypeimgs[imgindex];
+    imganimate = 1;
+//    window.alert("imgindex="+imgindex);
+
     document.body.nodes.add(imgtag);
     Util.abs(imgtag);
     Util.pos(imgtag, x, y);
@@ -39,10 +55,34 @@ class Bug {
     }  
     if (game.logo.y < y) {
       y -= speed;
-    }      
+    }
+    if (imganimate % 4 == 0) {
+      if (imgindex == 0) {
+        imgindex = 1;
+      } else if (imgindex == 1) {
+        imgindex = 0;
+      } else if (imgindex == 2) {
+        imgindex = 3;
+      } else if (imgindex == 3) {
+        imgindex = 2;
+      } else if (imgindex == 4) {
+        imgindex = 5;
+      } else if (imgindex == 5) {
+        imgindex = 4;
+      } else if (imgindex == 6) {
+        imgindex = 7;
+      } else if (imgindex == 7) {
+        imgindex = 6;
+      }
+//    window.alert("imgindex="+imgindex);
+    
+      imganimate = 0;
+    }
+    imgtag.attributes['src'] = bugtypeimgs[imgindex];
+    imganimate = imganimate + 1;
     Util.pos(imgtag, x, y);
   }
-  
+
   click() {
     imgtag.remove();
     game.score += 10;
